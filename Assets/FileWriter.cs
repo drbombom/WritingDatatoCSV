3a29beb8807b9767989038a986b6aa84a0dbf982
drbombom <85783815+drbombom@users.noreply.github.com>
2021-06-12 16:49:34 +0800

    First Commit to Main

---
+++ Assets/FileWriter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class FileWriter : MonoBehaviour {
+	public static FileWriter Instance;
+	public String fileName;
+	StreamWriter outStream;
+	public string[] Title;
+
+
+	void Awake ()
+	{
+		if(Instance == null){
+			Instance = this;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		fileName = String.Format("{0:yyyyMMdd-HHmmss}.csv", DateTime.Now);
+		outStream = System.IO.File.CreateText(fileName);
+		outStream.Write("Time");
+		for (int i=0;i<Title.Length;i++){
+			outStream.Write(","+Title[i]);
+		}
+		outStream.WriteLine();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+
+	public void WriteFile(String str){
+        outStream.WriteLine(Time.time+","+str);
+		outStream.Flush();
+	}
+
+	void OnDisable(){
+        outStream.Close();//The file will be saved under the project folder
+	}
+}
---
+++ Assets/RecordData.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecordData : MonoBehaviour {
+
+	// Use this for initialization
+	public FileWriter FW;
+	public string Data;
+	public bool Flag;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Data = transform.position.x.ToString();
+		Data += ","+transform.position.y.ToString();
+		Data += ","+transform.position.z.ToString();
+		Data += ","+Flag.ToString();
+		FW.WriteFile(Data);//Write string Data as a line of data
+	}
+}
